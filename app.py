e4b4c5086a9866a54bbd4fa461fa02f40c0f7319
Rakesh Vardan <rakeshlearnit@gmail.com>
2025-07-07 20:47:53 +0530

    Correcting the URL

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ app = FastAPI()
 # Enable CORS for all origins (customize as needed)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://rakeshvardan.com", "https://www.rakeshvardan.com/"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://rakeshvardan.com", "https://www.rakeshvardan.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
